40c64e8747dd1b0f479240d2ec9d68a77b50d477
KTGamage <kasuntharaka18628@gmail.com>
2025-09-05 21:02:40 +0530

    Create admin dashboard page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
@@ -125,4 +125,4 @@
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 </body>
-</html>
+</html> -->
